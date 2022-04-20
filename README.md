ff767f7c4cfc4383f54dc361160d72e2d9ac5b65
Kyle Gross <kyg555@outlook.com>
2022-04-19 18:01:10 -0700

    Add some functions and update README

--- README.md
+++ README.md
@@ -1,41 +1,64 @@
-# Discord Bot for Travian
+# Travian Bot
 
 To help automate leadership tasks.
 
 ## How to use
 
 * Required roles:
-  * `Admin`: to create countdowns and create artifact rotations.
-  * `def`: to be mentioned for countdowns.
+  * `Bot Master`: to create countdowns and artifact rotations.
+  * If you wish to create countdowns, please first set the defense role for your server with the `!def_role` command.
 
-### Commands
+## Commands
 
-#### `!countdown`
+|Command|Bot Master Role?|
+|--------|:--------------:|
+|[`!countdown`](https://github.com/kyle-gross/discord-bot#countdown)|YES|
+|[`!commit`](https://github.com/kyle-gross/discord-bot#commit)|NO|
+|[`!def_role`](https://github.com/kyle-gross/discord-bot#def_role)|YES|
 
-* args:
-  * `village-name`: name of village to defend. The created channel will be named countdown-`village_name`. If `village-name` contains spaces, The command will not work. Use hyphens to separate words.
-  * `amount`: integer amount of the required troops. Must be a number.
-  * `time`: expiration time of defense call.
-  * `link`: link to village to defend
+---
 
-* Example: ```!countdown village-name 100000 00:00:00 <link>```
+### `!countdown`
 
-* Used to create countdowns for defense calls. This command will create the countdown channel within the category "countdown". If the category does not exist, it will be made.
+Used to create countdowns for defense calls. This command will create the countdown channel within the category "countdown". If the category does not exist, it will be made.
 
-* The purpose of this channel is to count down the number of troops being committed to a defense call until the countdown has reached 0. When the countdown reaches 0, the channel permissions will be adjusted and messages will no longer be able to be sent there. The channel will be automatically removed 24 hours after the countdown expires.
+The purpose of the created channel is to count down the number of troops being committed to a defense call until the countdown has reached 0. When the countdown reaches 0, the channel permissions will be adjusted and messages will no longer be able to be sent there. The channel will be automatically removed 24 hours after the countdown expires.
 
-#### `!commit`
+**Example:** `!countdown village-name 100000 00:00:00 <link>`
 
-* args:
-  * `amount`: integer amount of the number of troops being sent.
+> * `village-name`: name of village to defend. The created channel will be named countdown-`village_name`. If `village-name` contains spaces, The command will not work. Use hyphens to separate words.
+> * `amount`: integer amount of the required troops. Must be a number.
+> * `time`: expiration time of defense call.
+> * `link`: link to village to defend
 
-* Example: ```!commit 10000```
+---
 
-* User should `!commit` the exact number of troops sent. No abbreviations, such as 10k instead of 10000.
+### `!commit`
 
-### Features still under development
+Command used to commit the exact number of troops sent to countdowns.
 
-* Artifact rotation manager:
-  * Oversees artifact rotations for trainer and storage artifacts.
+`!commit` is intended to take an exact number, therefore do not use abbreviations. ~~`10k`~~ should be `10000`.
 
-  * Users will be able to request artifacts and be added to wait-lists to use artifacts.
+**Example:** `!commit "amount""`
+
+> * `amount`: integer amount of the number of troops being sent.
+
+---
+
+### `!def_role`
+
+Command used to set the defense role for your particular server. This role will be @ mentioned when creating countdowns, therefore it is required to set this role before creating countdowns.
+
+**Example:** `!def_role "role name"`
+
+> * `role name`: the role specified here will become the defense role for your server.
+>   * To change the defense role, simply run the command again with the correct role.
+
+---
+
+## Coming soon
+
+Artifact rotation manager:
+
+* Oversees artifact rotations for trainer and storage artifacts.
+* Users will be able to request artifacts and be added to wait-lists to use artifacts.
--- app.py
+++ app.py
@@ -2,23 +2,85 @@ import asyncio
 from dotenv import load_dotenv
 import json
 import nextcord
+from nextcord import Embed
 from nextcord.ext import commands
 import os
 
 load_dotenv()
-
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='!')
-possible_artifacts = ['small-trainer', 'large-trainer', 'small-storage', 'large-storage']
 
+intents = nextcord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+possible_artifacts = [
+    'small-trainer', 'large-trainer', 'unique-trainer',
+    'small-storage', 'large-storage'
+]
+
+
+# Command for setting the defense role for the server.
+# This is required for creating countdowns
+@commands.has_role('Bot Master') # Requires Bot Master role
+@bot.command(name='def_role')
+async def def_role(ctx, def_role:str):
+    role_path = './json_files/server_info.json'
+
+    server_info = {ctx.guild.name: {'def_role': def_role}}
+
+    with open(role_path, 'w') as json_file:
+        json.dump(server_info, json_file)
+
+    embed = Embed(
+        title=f'Defense role has been set: {def_role}',
+        description='If this is incorrect, run the `!def_role` command again and specify the proper defense role.',
+        color=nextcord.Color.green()
+    )
+    embed.add_field(
+        name='Notes:',
+        value='• Use the exact role name as it appears in your server. This is case sensitive.'
+    )
+    await ctx.reply(embed=embed)
+
+@def_role.error
+async def def_role_error(ctx, error):
+    if not isinstance(error, commands.MissingRole):
+        print(error)
+        embed = Embed(
+            title='Incorrect format',
+            description='Please follow this example:\n' +
+            '`!def_role "role"`',
+            color=nextcord.Color.green()
+        )
+        embed.add_field(
+            name='Notes:',
+            value='• `"role"` is the name of the defense role within your specific Discord server.'
+        )
+        await ctx.reply(embed=embed)
 
 # Command is used to create a def call countdown channel
 # Input format: !countdown "name" "amount" "time" "village link"
-@commands.has_role('Admin') # Command requires Admin role
+@commands.has_role('Bot Master') # Command requires Bot Master role
 @bot.command(name='countdown')
 async def countdown(ctx, name:str, troops:int, time:str, link:str):
     print(f'{ctx.author} has requested a countdown:', name, troops, time, link)
     json_path = './json_files/active_calls.json'
+    role_path = './json_files/server_info.json'
+
+    # Ensure that the server has a designated def_role
+    with open(role_path) as json_file:
+        server_info = json.load(json_file)
+        role_name = server_info[ctx.guild.name]['def_role']
+
+    if role_name is None:
+        embed = Embed(
+            title='Defense role not yet specified.',
+            description='To specify a defense role, run the command `!def_role "role"`\n' +
+            '• "role" is the name of the defense role within your specific Discord server.',
+            color=nextcord.Color.green()
+        )
+        await ctx.reply(embed=embed)
+        return
 
     # Create new countdown channel
     countdown_name = 'countdown-' + name
@@ -28,7 +90,7 @@ async def countdown(ctx, name:str, troops:int, time:str, link:str):
     channel = await ctx.guild.create_text_channel(countdown_name, category=category)
     print(f'Created new channel id: {channel.id}')
 
-    embed = nextcord.Embed(
+    embed = Embed(
         title='Countdown channel has been created.',
         description=f'{channel.mention}',
         color=nextcord.Color.green()
@@ -36,8 +98,8 @@ async def countdown(ctx, name:str, troops:int, time:str, link:str):
     await ctx.reply(embed=embed)
 
     # Message to be sent in new channel
-    role = nextcord.utils.get(ctx.guild.roles, name='def')
-    embed = nextcord.Embed(title=f'🛡️ ⚔️ NEW DEFENSE CALL ⚔️ 🛡️', description=f'{role.mention}', color=nextcord.Color.green())
+    role = nextcord.utils.get(ctx.guild.roles, name=role_name)
+    embed = Embed(title=f'🛡️ ⚔️ NEW DEFENSE CALL ⚔️ 🛡️', description=f'{role.mention}', color=nextcord.Color.green())
     embed.add_field(name=f'Troops needed:', value=f'{troops}', inline=True)
     embed.add_field(name=f'Before:', value=f'{time}', inline=True)
     embed.add_field(name=f'Send troops to:', value=f'{link}', inline=True)
@@ -61,10 +123,10 @@ async def countdown(ctx, name:str, troops:int, time:str, link:str):
 async def countdown_error(ctx, error):
     if not isinstance(error, commands.MissingRole):
         print(error)
-        embed = nextcord.Embed(
+        embed = Embed(
             title='Incorrect format',
             description='Please follow this example:\n' +
-                '`!countdown "name-of-channel" "amount" "time" "link to village"`',
+                        '`!countdown "name-of-channel" "amount" "time" "link to village"`',
             color=nextcord.Color.green()
         )
         embed.add_field(
@@ -93,7 +155,7 @@ async def commit(ctx, troops:int):
 
         # If the call is not full
         if remaining > 0:
-            embed = nextcord.Embed(
+            embed = Embed(
                 title=f'{ctx.author.name} has committed {troops} troops.',
                 description=f'**{remaining}** remaining before **{time}**\n{link}',
                 color=nextcord.Color.green()
@@ -105,7 +167,7 @@ async def commit(ctx, troops:int):
 
         # If the call is filled, close comments and delete channel after 24 hours
         else:
-            embed = nextcord.Embed(
+            embed = Embed(
                 title='🛡️ ⚔️ This defense call has been filled! ⚔️ 🛡️',
                 description='This channel is now closed and will be deleted in 24 hours.',
                 color=nextcord.Color.green()
@@ -119,12 +181,13 @@ async def commit(ctx, troops:int):
             active_calls.pop(channel_name)
             with open(json_path, 'w') as json_file:
                 json.dump(active_calls, json_file)
+
             # Delete channel after 24 hours
-            await asyncio.sleep(30) # 86400 seconds in 1 day
+            await asyncio.sleep(86400) # 86400 seconds in 1 day
             print(f'{channel_name} has been deleted.')
             await ctx.channel.delete()
 
-@commands.has_role('Admin') # Command requires Admin role
+@commands.has_role('Bot Master') # Command requires Bot Master role
 @bot.command(name='add_artifact')
 async def add_artifact(ctx, artifact:str, hours:int):
     json_path = './json_files/artifacts.json'
@@ -145,19 +208,8 @@ async def add_artifact(ctx, artifact:str, hours:int):
 @add_artifact.error
 async def add_artifact_error(ctx, error):
     # If the user has chosen an artifact which is not in the list of possible artifacts
-    if isinstance(error, commands.BadArgument):
-        embed = nextcord.Embed(
-            title='The artifact requested is not available',
-            color=nextcord.Color.dark_gold()
-        )
-        embed.add_field(
-            name='Available artifacts are:',
-            value='• ' + '\n• '.join(possible_artifacts),
-            inline=False
-        )
-        await ctx.reply(embed=embed)
-    # If user has the required role, but has thrown an error
-    elif not isinstance(error, commands.MissingRole):
+    # Or if user has the required role, but has thrown an error
+    if not isinstance(error, commands.MissingRole):
         embed = nextcord.Embed(
             title='Incorrect format',
             description='Please follow this example:\n`!add_artifact "artifact-name" "number of hours"`',
@@ -167,9 +219,43 @@ async def add_artifact_error(ctx, error):
             name='Notes:',
             value='• "artifact-name" must be dash seperated.\n' +
                   '• "number of hours" is the number of hours this artifact may be held before the owner is notified to give to the next person.',
-                  inline=False
+                  inline=True
+        )
+        embed.add_field(
+            name='Available artifacts are:',
+            value='• ' + '\n• '.join(possible_artifacts),
+            inline=True
         )
         await ctx.reply(embed=embed)
+
+@bot.event
+async def on_guild_join(ctx):
+    print(f'{ctx.owner} has invited {bot.user.name} to the server: {ctx}')
+
+    json_path = './json_files/server_info.json'
+    server_info = {ctx.name: {'def_role': None}}
+
+    with open(json_path, 'w') as json_file:
+        json.dump(server_info, json_file)
+
+    embed = Embed(
+        title=f'Thank you for inviting {bot.user.name} to your server!',
+        color=nextcord.Color.dark_gold()
+    )
+    embed.add_field(
+        name='Tips:',
+        value='• It is recommended to create a private channel for writing bot commands, so that general channels are not spammed with bot responses.\n' +
+        '• To use some bot commands (such as `!countdown`), the `Bot Master` role is required.\n' +
+        '• Ensure that all users who are allowed to manage the bot are assigned the role `Bot Master`.\n' +
+        '• Please run the `!def_role` command to set the name of your defender role for your server. ex: `Def`, `Defender`, `Defense`\n' +
+        '• The `Bot Master` role is required for the `!def_role` command!',
+        inline=False
+    )
+    embed.add_field(
+        name='For a full list of commands, please refer to the bot documentation',
+        value='https://github.com/kyle-gross/discord-bot/blob/master/README.md'
+    )
+    await ctx.owner.send(embed=embed)
 
 @bot.event
 async def on_ready():
